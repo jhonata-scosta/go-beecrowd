01a0212e1aecfef7267ba3b366015716019448d6
Jhonata Souza <jhonata.scosta@proton.me>
2026-01-24 19:45:43 -0300

    Submitting resolution 1004

---
+++ 1004 - Produto Simples/main.go
@@ -0,0 +1,12 @@
+// Link => https://resources.beecrowd.com/repository/UOJ_1004.html
+package main
+
+import "fmt"
+
+func main(){
+	var A, B int
+	fmt.Scanln(&A)
+	fmt.Scanln(&B)
+	PROD := A * B
+	fmt.Printf("PROD = %d\n", PROD)
+}
