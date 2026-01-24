980fcac36421b3947b7257dd4922e147ccddd51a
Jhonata Souza <jhonata.scosta@proton.me>
2026-01-24 19:45:01 -0300

    Submitting resolution 1003

---
+++ 1003 - Soma Simples/main.go
@@ -0,0 +1,12 @@
+// Link => https://resources.beecrowd.com/repository/UOJ_1003.html
+package main
+
+import "fmt"
+
+func main() {
+	var A, B int
+	fmt.Scanln(&A)
+	fmt.Scanln(&B)
+	soma := A + B
+	fmt.Printf("SOMA = %d\n", soma)
+}
