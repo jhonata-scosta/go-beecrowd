cb77d3cfb935dd38f73033e2bff536e78c78e0b1
Jhonata Souza <jhonata.scosta@proton.me>
2026-01-23 21:18:13 -0300

    Submitting resolution 1001

---
+++ 1001 - Extremamente Básico/main.go
@@ -0,0 +1,12 @@
+// Link => https://resources.beecrowd.com/repository/UOJ_1001.html
+package main
+
+import "fmt"
+
+func main () {
+	var A, B int
+	fmt.Scanln(&A)
+	fmt.Scanln(&B)
+	x := A + B
+	fmt.Println("X =",x)
+}
