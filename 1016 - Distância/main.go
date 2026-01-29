e425a501dc022fc849e689b19e07b5b72bf408a4
Jhonata Souza <jhonata.scosta@proton.me>
2026-01-29 18:16:50 -0300

    Submitting resolution 1016

---
+++ 1016 - Distância/main.go
@@ -0,0 +1,12 @@
+// Link => https://resources.beecrowd.com/repository/UOJ_1016.html
+package main
+
+import "fmt"
+
+func main(){
+	var kmpm int = 2
+	var km int
+	fmt.Scanf("%d",&km)
+	distancia := kmpm * km
+	fmt.Printf("%d minutos\n", distancia)
+}
