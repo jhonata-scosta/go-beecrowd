29c2dab274f1d2b04e9853a3df170476d5e12057
Jhonata Souza <jhonata.scosta@proton.me>
2026-02-03 21:31:06 -0300

    Submitting resolution 2413

---
+++ 2413 - Busca na Internet/main.go
@@ -0,0 +1,10 @@
+// Link: https://resources.beecrowd.com/repository/UOJ_2413.html
+package main
+
+import "fmt"
+
+func main(){
+  var input int
+  fmt.Scanf("%d", &input)
+  fmt.Println(input*4)
+}
