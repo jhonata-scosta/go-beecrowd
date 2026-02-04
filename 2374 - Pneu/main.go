b4ba313e8f7d2a6a17ffff973d001538c9c16930
Jhonata Souza <jhonata.scosta@proton.me>
2026-02-03 21:30:52 -0300

    Submitting resolution 2374

---
+++ 2374 - Pneu/main.go
@@ -0,0 +1,11 @@
+// Link: https://resources.beecrowd.com/repository/UOJ_2374.html
+package main
+
+import "fmt"
+
+func main(){
+  var n, m int
+  fmt.Scanf("%d", &n)
+  fmt.Scanf("%d", &m)
+  fmt.Println(n - m)
+}
